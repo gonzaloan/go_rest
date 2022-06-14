d01c16a80cab54bdc27fd115eeac7267a56d9680
Gonzalo Muñoz <gonzalo.munoz0@walmart.com>
2022-06-14 16:49:02 -0400

    Adds crud to post

--- database/up.sql
+++ database/up.sql
@@ -6,3 +6,11 @@ CREATE TABLE users (
     created_at TIMESTAMP NOT NULL DEFAULT NOW()
 
 );
+DROP TABLE IF EXISTS "posts";
+CREATE TABLE posts (
+    id VARCHAR(32) PRIMARY KEY,
+    post_content VARCHAR(32) NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    user_id VARCHAR(32) NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
